e15afd5f92e9e973c6a4c9b66535f7b4b1531225
Rodrigo <rodrigocosta47@outlook.com>
2020-07-14 23:10:32 -0300

    criei um teste unico para avalar a classe Avalaidador e testra se ha alum big

---
+++ test_dominio.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+
+class TestAvaliador(TestCase):
+    def test_avalia_lance(self):
+        from leilao.dominio import Usuario, Leilao, Lance, Avaliador
+
+        rodrigo = Usuario("rodrigo")
+        pablo = Usuario('pablo')
+
+        lance_pablo = Lance("pablo", 100)
+        lance_rodrigo = Lance("rodrigo", 150)
+
+        leilao = Leilao('Celular')
+
+        leilao.lances.append(lance_pablo)
+        leilao.lances.append(lance_rodrigo)
+
+        avaliador = Avaliador()
+
+        avaliador.avalia_lance(leilao)
+
+        valor_esperado_maior = 150
+        valor_esperado_menor = 100
+
+        self.assertEqual(valor_esperado_maior, avaliador.maior_lance)
+        self.assertEqual(valor_esperado_menor, avaliador.menor_lance)
